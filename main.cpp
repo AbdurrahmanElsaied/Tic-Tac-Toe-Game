f2ed3240920735c729fb583b02acd33cc8de3c33
Abdurrahman Elsaied <aoka48212@gmail.com>
2025-09-19 01:31:31 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define oo INT_MAX
+#define ooL LLONG_MAX
+#define mod 1000000007
+#define fix(x, m) (((x) % (m) + (m)) % (m))
+void A() {
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt.txt", "r", stdin);
+    freopen("Output.txt.txt", "w", stdout);
+#else
+    //freopen("justice.in", "r", stdin);
+    //freopen("bridges.out", "w", stdout);
+#endif
+}
+
+signed main() {
+    A();
+    int n;cin >> n;
+    cout << n;
+
+
+
+    return 0;
+}
