04ed24630c78dfc01e038a1b3c6c09d8d8e994a0
Abdurrahman Elsaied <aoka48212@gmail.com>
2025-09-19 01:33:17 +0300

    TicTacToe.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,110 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define all(x) x.begin(), x.end()
-#define oo INT_MAX
-#define ooL LLONG_MAX
-#define mod 1000000007
-#define fix(x, m) (((x) % (m) + (m)) % (m))
-void A() {
-    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-#ifndef ONLINE_JUDGE
-    freopen("Input.txt.txt", "r", stdin);
-    freopen("Output.txt.txt", "w", stdout);
-#else
-    //freopen("justice.in", "r", stdin);
-    //freopen("bridges.out", "w", stdout);
-#endif
+
+vector<vector<char>> board = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+
+int choice;
+int row,column;
+char turn = 'X';
+bool draw = false;
+void display_board() {
+    cout << "              PLAYER - 1 [X] \t PLAYER - 2 [O]\n\n";
+
+    for(int i = 0; i < 3; i++) {
+        cout << "   " << board[i][0] << "   |   " << board[i][1] << "   |   " << board[i][2] << "\n";
+
+        if(i < 2) {
+            cout << "_______|_______|_______\n";
+        }
+
+    }
+    cout << "\n";
 }
 
-signed main() {
-    A();
-    int n;cin >> n;
-    cout << n;
+void player_turn(){
+    if(turn == 'X'){
+        cout<<"Player - 1 [X] turn : ";
+    }
+    else if(turn == 'O'){
+        cout<<"Player - 2 [O] turn : ";
+    }
 
 
+    cin>> choice;
 
-    return 0;
+
+    switch(choice){
+        case 1: row=0; column=0; break;
+        case 2: row=0; column=1; break;
+        case 3: row=0; column=2; break;
+        case 4: row=1; column=0; break;
+        case 5: row=1; column=1; break;
+        case 6: row=1; column=2; break;
+        case 7: row=2; column=0; break;
+        case 8: row=2; column=1; break;
+        case 9: row=2; column=2; break;
+        default:
+            cout<<"Invalid Move";
+    }
+
+    if(turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O'){
+
+        board[row][column] = 'X';
+        turn = 'O';
+
+    }
+    else if(turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O'){
+
+        board[row][column] = 'O';
+        turn = 'X';
+    }
+    else {
+        cout<<"Box already filled!n Please choose another!!";
+        player_turn();
+    }
+    display_board();
+}
+
+
+bool gameover(){
+
+    for(int i=0; i<3; i++)
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
+            return false;
+
+
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
+        return false;
+
+
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            if(board[i][j] != 'X' && board[i][j] != 'O')
+                return true;
+        }
+    }
+
+    draw = true;
+    return false;
+}
+
+signed main()
+{
+    cout<<"          TIC   TAC  TOE  GAME!!!!\n";
+    cout<< "=======================================================\n";
+    while(gameover()){
+        display_board();
+        player_turn();
+        gameover();
+    }
+    if(turn == 'X' && draw == false){
+        cout<<"Congratulations!!!!!!!!!!!!!!!!!\n Player with 'O' has won the game!\n";
+    }
+    else if(turn == 'O' && draw == false){
+        cout<<"Congratulations!!!!!!!!!!!!!!!!!\n Player with 'X' has won the game!\n";
+    }
+    else
+    cout<<"GAME DRAW!!!\n";
 }
